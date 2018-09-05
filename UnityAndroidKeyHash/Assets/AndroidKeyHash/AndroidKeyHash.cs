5e934d35d667d80de1daccd7b456512d7c503d83
trinhvietvan <trinhvietvan@gmail.com>
2018-09-05 15:52:33 +0700

    Add files via upload

---
+++ UnityAndroidKeyHash/Assets/AndroidKeyHash/AndroidKeyHash.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidKeyHash
+{
+    private static AndroidJavaClass pluginClass;
+    private static AndroidJavaObject activity;
+
+    public static string KeyHash()
+    {
+        string key = string.Empty;
+
+        if (pluginClass == null || activity == null)
+        {
+            AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            pluginClass = new AndroidJavaClass("van.nzt.UnityAndroidKeyHash");
+        }
+
+        key = pluginClass.CallStatic<string>("GetKeyHash", activity);
+
+        return key;
+    }
+}
